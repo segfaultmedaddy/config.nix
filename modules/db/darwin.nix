26815165a2f6c27d2b11ac14209cd2840bde250f
Roman Vanesyan <roman@vanesyan.com>
2024-03-31 00:23:19 +0200

    chore(nix): install postgresql lib from homebrew as Rails works only with it

--- modules/db/darwin.nix
+++ modules/db/darwin.nix
@@ -1,5 +1,11 @@
-{config, ...}: {
+{ config, ... }: {
   homebrew = {
+    brews = [
+      # This is needed for the postgres client to work.
+      # I choose it over nix package cause Rails has troubles with it.
+      "postgresql"
+    ];
+
     casks = [
       "beekeeper-studio"
     ];
--- modules/db/home.nix
+++ modules/db/home.nix
@@ -1,7 +1,6 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   home.packages = with pkgs; [
     atlas # atlas is a tool for db migrations
     sqlc # sqlc is a tool for generating type safe Go from SQL
-    postgresql # only for CLI tools
   ];
 }
