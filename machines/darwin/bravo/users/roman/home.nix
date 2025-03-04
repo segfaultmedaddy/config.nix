7bf23a75bd2fd47cb9fb44b76f3f94486df14c60
Roman Vanesyan <roman@vanesyan.com>
2025-03-04 13:52:07 +0200

    feat: bravo deps

--- machines/darwin/bravo/users/roman/home.nix
+++ machines/darwin/bravo/users/roman/home.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   home.stateVersion = "24.11";
+
+  home.packages = with pkgs; [
+    pre-commit
+  ];
 
   home.sessionVariables = {
     CONFIG_ROOT_DIR = "$HOME/dev/personal/config.nix";
