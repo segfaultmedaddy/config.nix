e9e067d68ba5ba6b9ac014bcde4cb49099da597e
Roman Vanesyan <roman@vanesyan.com>
2025-08-31 17:44:59 +0300

    feat: add video & audio

--- machines/linux/echo/default.nix
+++ machines/linux/echo/default.nix
@@ -11,6 +11,26 @@
     bashInteractive
   ];
 
+  # Enable wayland
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "amdgpu" ];
+
+  # Enable sound
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    jack.enable = true;
+  };
+
+  # Enable input devices
+  services.libinput.enable = true;
+
+  # Enable display manager
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
+  services.displayManager.defaultSession = "hyprland";
+
   networking.networkmanager.enable = true;
   services.getty.autologinUser = "${user}";
 }
