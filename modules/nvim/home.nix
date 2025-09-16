9e066bdb7014180ed7957441d65b54bdf05dd0c6
Roman Vanesyan <roman@vanesyan.com>
2025-09-16 09:40:16 +0000

    fix: fix fzf

--- modules/nvim/home.nix
+++ modules/nvim/home.nix
@@ -8,7 +8,7 @@
     extraPackages = with pkgs; [
       gcc # tree-sitter dep.
       ripgrep # telescope dep.
-      cmake # fzf dep
+      gnumake # fzf dep
       fzf # telescope dep.
       fd # telescope dep.
       tree-sitter
